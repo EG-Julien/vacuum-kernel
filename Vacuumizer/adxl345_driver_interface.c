e3612443b1646d66e617ed890097cd44c9d752a7
Julien Geneste <julien@aircslab.fr>
2023-05-11 19:02:06 +0200

    adxl345 driver

--- Vacuumizer/adxl345_driver_interface.c
+++ Vacuumizer/adxl345_driver_interface.c
@@ -1,327 +1,56 @@
-/**
- * Copyright (c) 2015 - present LibDriver All rights reserved
- * 
- * The MIT License (MIT)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE. 
- *
- * @file      driver_adxl345_interface_template.c
- * @brief     driver adxl345 interface template source file
- * @version   2.0.0
- * @author    Shifeng Li
- * @date      2021-04-20
- *
- * <h3>history</h3>
- * <table>
- * <tr><th>Date        <th>Version  <th>Author      <th>Description
- * <tr><td>2021/04/20  <td>2.0      <td>Shifeng Li  <td>format the code
- * <tr><td>2020/12/23  <td>1.0      <td>Shifeng Li  <td>first upload
- * </table>
- */
-
 #include "adxl345_driver_interface.h"
 
-/**
- * Macro used to cs
-*/
-
-static inline void cs_select() {
-    asm volatile("nop \n nop \n nop");
-    gpio_put(PIN_CS, 0);  // Active low
-    asm volatile("nop \n nop \n nop");
-}
-
-static inline void cs_deselect() {
-    asm volatile("nop \n nop \n nop");
-    gpio_put(PIN_CS, 1);
-    asm volatile("nop \n nop \n nop");
-}
-
-/**
- * @brief  interface iic bus init
- * @return status code
- *         - 0 success
- *         - 1 iic init failed
- * @note   none
- */
-uint8_t adxl345_interface_iic_init(void)
+void i2c_write(int addr, uint8_t reg, uint8_t val)
 {
-    i2c_init(i2c_default, 100 * 1000); // defaulting i2c interface to i2c_default
-    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
-    // Make the I2C pins available to picotool
-    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
-
-    return 0;
+    uint8_t data[2];
+    data[0] = reg;
+    data[1] = val;
+    i2c_write_blocking(I2C_PORT, addr, data, sizeof(data), false);
 }
 
-/**
- * @brief  interface iic bus deinit
- * @return status code
- *         - 0 success
- *         - 1 iic deinit failed
- * @note   none
- */
-uint8_t adxl345_interface_iic_deinit(void)
+void i2c_read(int addr, uint8_t reg, size_t size, uint8_t *val)
 {
-    return 0;
+    i2c_write_blocking(I2C_PORT, addr, &reg, 1, false);
+    i2c_read_blocking(I2C_PORT, addr, val, size, false);
 }
 
-/**
- * @brief      interface iic bus read
- * @param[in]  addr is the iic device write address
- * @param[in]  reg is the iic register address
- * @param[out] *buf points to a data buffer
- * @param[in]  len is the length of the data buffer
- * @return     status code
- *             - 0 success
- *             - 1 read failed
- * @note       none
- */
-uint8_t adxl345_interface_iic_read(uint8_t addr, uint8_t reg, uint8_t *buf, uint16_t len)
+void init_adxl345()
 {
-    uint8_t err = 0;
 
-    err += i2c_write_blocking(i2c_default, addr, &reg, 1, true);
-    err += i2c_read_blocking(i2c_default, addr, buf, len, false);
-
-    if (err > 0)
-        return 0;
-
-    return 1;
-}
-
-/**
- * @brief     interface iic bus write
- * @param[in] addr is the iic device write address
- * @param[in] reg is the iic register address
- * @param[in] *buf points to a data buffer
- * @param[in] len is the length of the data buffer
- * @return    status code
- *            - 0 success
- *            - 1 write failed
- * @note      none
- */
-uint8_t adxl345_interface_iic_write(uint8_t addr, uint8_t reg, uint8_t *buf, uint16_t len)
-{
-    uint8_t buffer[len + 1];
-
-    for (uint8_t i = 0; i < (len + 1); i++)
-    {
-        if (i == 0) {
-            buffer[i] = reg;
-        } else {
-            buffer[i] = buf[i - 1];
-        }
-    }
-
-    uint8_t err = i2c_write_blocking(i2c_default, addr, buffer, (len + 1), false);
-    if (err > 0)
-        return 0;
-    return 1;
-}
-
-/**
- * @brief  interface spi bus init
- * @return status code
- *         - 0 success
- *         - 1 spi init failed
- * @note   none
- */
-uint8_t adxl345_interface_spi_init(void)
-{
-    spi_init(spi_default, 500 * 1000);
-    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-    // Make the SPI pins available to picotool
-    bi_decl(bi_3pins_with_func(PIN_MISO, PIN_MOSI, PIN_SCK, GPIO_FUNC_SPI));
-
-    // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_init(PIN_CS);
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
-    // Make the CS pin available to picotool
-    bi_decl(bi_1pin_with_name(PIN_CS, "SPI CS"));
-
-    return 0;
-}
-
-/**
- * @brief  interface spi bus deinit
- * @return status code
- *         - 0 success
- *         - 1 spi deinit failed
- * @note   none
- */
-uint8_t adxl345_interface_spi_deinit(void)
-{   
-    return 0;
-}
-
-/**
- * @brief      interface spi bus read
- * @param[in]  reg is the register address
- * @param[out] *buf points to a data buffer
- * @param[in]  len is the length of data buffer
- * @return     status code
- *             - 0 success
- *             - 1 read failed
- * @note       none
- */
-uint8_t adxl345_interface_spi_read(uint8_t reg, uint8_t *buf, uint16_t len)
-{
-    reg |= READ_BIT;
-    cs_select();
-    spi_write_blocking(spi_default, &reg, 1);
-    sleep_ms(10);
-    spi_read_blocking(spi_default, 0, buf, len);
-    cs_deselect();
-    sleep_ms(10);
-    return 0;
-}
-
-/**
- * @brief     interface spi bus write
- * @param[in] reg is the register address
- * @param[in] *buf points to a data buffer
- * @param[in] len is the length of data buffer
- * @return    status code
- *            - 0 success
- *            - 1 write failed
- * @note      none
- */
-uint8_t adxl345_interface_spi_write(uint8_t reg, uint8_t *buf, uint16_t len)
-{
-    uint8_t buffer[len + 1];
-
-    for (uint8_t i = 0; i < (len + 1); i++)
-    {
-        if (i == 0) {
-            buffer[i] = reg;
-        } else {
-            buffer[i] = buf[i - 1];
-        }
-    }
-
-    cs_select();
-    spi_write_blocking(spi_default, buffer, (len + 1));
-    return 0;
-}
-
-/**
- * @brief     interface delay ms
- * @param[in] ms
- * @note      none
- */
-void adxl345_interface_delay_ms(uint32_t ms)
-{
-    sleep_ms(ms);
-}
-
-/**
- * @brief     interface print format data
- * @param[in] fmt is the format data
- * @note      none
- */
-void adxl345_interface_debug_print(const char *const fmt, ...)
-{
-    va_list ap;
-    int ret;
-    char buffer[64];
-
-    va_start(ap, fmt);
-    ret = vsprintf(buffer, fmt, ap);
-    va_end(ap);
+    i2c_init(I2C_PORT, 400*1000);
     
-    for (uint8_t i = 0; i < 64; i++)
-    {
-        if (buffer[i] == 0)
-            return;
-        uart_putc(uart0, buffer[i]);
-    }
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    uint8_t data = 0;
+
+    i2c_write(ADXL345_ADDRESS, 0x31, 0x0B);   // 13-bit mode  +_ 16g
+    i2c_write(ADXL345_ADDRESS, 0x2D, 0x08);   // Power register
+
+
+    i2c_write(ADXL345_ADDRESS, 0x1E, 0x00);   // x
+    i2c_write(ADXL345_ADDRESS, 0x1F, 0x00);   // Y
+    i2c_write(ADXL345_ADDRESS, 0x20, 0x05);   // Z
+
+    // Check to see if it worked!
+    i2c_read(ADXL345_ADDRESS, 0X00, 1, &data);
+    if(data==0xE5)
+        printf("Success\n");
+    else
+        printf("Fail\n");
 }
 
-/**
- * @brief     interface receive callback
- * @param[in] type is the irq type
- * @note      none
- */
-void adxl345_interface_receive_callback(uint8_t type)
+void read_adxl345(uint16_t *accelerometer_data) 
 {
-    switch (type)
-    {
-        case ADXL345_INTERRUPT_DATA_READY :
-        {
-            adxl345_interface_debug_print("adxl345: irq data ready.\n");
-            
-            break;
-        }
-        case ADXL345_INTERRUPT_SINGLE_TAP :
-        {
-            adxl345_interface_debug_print("adxl345: irq single tap.\n");
-            
-            break;
-        }
-        case ADXL345_INTERRUPT_DOUBLE_TAP :
-        {
-            adxl345_interface_debug_print("adxl345: irq double tap.\n");
-            
-            break;
-        }
-        case ADXL345_INTERRUPT_ACTIVITY :
-        {
-            adxl345_interface_debug_print("adxl345: irq activity.\n");
-            
-            break;
-        }
-        case ADXL345_INTERRUPT_INACTIVITY :
-        {
-            adxl345_interface_debug_print("adxl345: irq inactivity.\n");
-            
-            break;
-        }
-        case ADXL345_INTERRUPT_FREE_FALL :
-        {
-            adxl345_interface_debug_print("adxl345: irq free fall.\n");
-            
-            break;
-        }
-        case ADXL345_INTERRUPT_WATERMARK :
-        {
-            adxl345_interface_debug_print("adxl345: irq water mark.\n");
-            
-            break;
-        }
-        case ADXL345_INTERRUPT_OVERRUN :
-        {
-            adxl345_interface_debug_print("adxl345: irq overrun.\n");
-            
-            break;
-        }
-        default :
-        {
-            adxl345_interface_debug_print("adxl345: unknown code.\n");
-            
-            break;
-        }
+    uint8_t bytes[6];
+    memset(bytes,0,6);
+
+    // Read 6 bytes from the ADXL345
+    i2c_read(ADXL345_ADDRESS, ADXL345_REGISTER_XLSB, 6, bytes);
+    // Unpack data
+    for (int i=0;i<3;++i) {
+        accelerometer_data[i] = (int16_t)bytes[2*i] + (((int16_t)bytes[2*i + 1]) << 8);
     }
 }
--- main.c
+++ main.c
@@ -15,7 +15,6 @@
 
 #include "Vacuumizer/pico_uart_transports.h"
 #include "Vacuumizer/Vacuumizer.h"
-#include "Vacuumizer/adxl345_driver.h"
 #include "Vacuumizer/adxl345_driver_interface.h"
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc); return 1;}}
@@ -68,29 +67,26 @@ int main(int argc, const char * const * argv)
 
 	uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
 
-	uart_print("firmware_version %d\n", FIRMWARE_VERSION);
+	uart_print("firmware_version %0.2f\n", FIRMWARE_VERSION);
+	uart_print("peripherals initializing !");
+	
+	static int16_t accelerometer_data[3];
+	memset(accelerometer_data, 0, sizeof(accelerometer_data));
+	init_adxl345();
 
-	uint8_t res, i;
-	float g[3];
-
-	res = adxl345_basic_init(ADXL345_INTERFACE_IIC, ADXL345_ADDRESS_ALT_0);
-
-	for (i = 0; i < 3; i++)
-	{
-		res = adxl345_basic_read((float *)g);
-
-		if (res != 0)
-		{			
-			return 1;
-		}
-		
-		adxl345_interface_debug_print("adxl345: x is %0.3f.\n", g[0]);
-		adxl345_interface_debug_print("adxl345: y is %0.3f.\n", g[1]);
-		adxl345_interface_debug_print("adxl345: z is %0.3f.\n", g[2]);
-		adxl345_interface_delay_ms(1000);	
-	}
-
-	return 0;
+	uart_print("peripherals initialized !");
+    
+	while(1)
+    {
+        read_adxl345(accelerometer_data);
+        //3.9mg/LSB scale factor in 13-bit mode
+        uart_print("RAW: %d\t%d\t%d\n",accelerometer_data[0], accelerometer_data[1], accelerometer_data[2]);
+        uart_print("ACCEL: %f\t%f\t%f\n",
+                        ((float)accelerometer_data[0])*3.9/1000,
+                        ((float)accelerometer_data[1])*3.9/1000,
+                        ((float)accelerometer_data[2])*3.9/1000);
+        sleep_ms(10);
+    }
 
   	rcl_allocator_t allocator = rcl_get_default_allocator();
 	rclc_support_t support;
